38509e8cc48b70d10f83f42e2a0bdd65cc9f4df3
Eduardo Elias <du7@msn.com>
2019-01-12 10:59:32 +0100

    Declaration

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from './interfaces/IPiece';
 export * from './interfaces/IPipeFactory';
 export * from './interfaces/IPiper';
 export * from './Piper';
+export * from './__tests__/Piper.test';
